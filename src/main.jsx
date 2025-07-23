b674d27317e5429d049df146c20582a300302db6
Your Namecd <shaharulsiyam0273@gmail.com>
2025-07-23 16:04:46 +0600

    redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from 'react-redux';
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { store } from './Redux/store.js';
 import { Routes } from "./routes/Routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={Routes} />
+    <Provider store={store}>
+      <RouterProvider router={Routes} />
+    </Provider>
   </StrictMode>
 );
